87a2640f8cdffafddb76741302c6db7a9bacfdc1
Stardust <52759497+EHammons@users.noreply.github.com>
2019-10-15 19:27:50 -0500

    commiting everything, sorry....

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Urkle",
+		photo: "https://goinswriter.com/wp-content/uploads/2012/03/not-funny-570x378.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Jeff",
+		photo: "https://www.tvinsider.com/wp-content/uploads/2019/07/NUP_186843_0208-750x570.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Chrissy",
+		photo: "https://www.sheknows.com/wp-content/uploads/2019/08/chrissy-teigen-1.jpg?resize=768,432",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Kenan",
+		photo: "https://grantland.com/wp-content/uploads/2014/08/kenan-thompson-snlweek.jpg?w=1024",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Kel",
+		photo: "https://vignette.wikia.nocookie.net/livandmaddie/images/2/25/Kel_Mitchell.jpg/revision/latest?cb=20140311200013",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
